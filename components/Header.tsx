0179a64aa04d4d9dc161276f0d79b7b289faf857
M. Rehan Abbasi <mrehanabbasi@yahoo.com>
2022-10-29 13:09:46 +0500

    add addItem page

--- components/Header.tsx
+++ components/Header.tsx
@@ -39,7 +39,7 @@ function Header({}: Props) {
           <p className="headerLink">Sell</p>
           <p className="headerLink">Watchlist</p>
 
-          <Link href="/addItems" className="flex items-center hover:link">
+          <Link href="/addItem" className="flex items-center hover:link">
             Add to inventory
             <ChevronDownIcon className="h-4" />
           </Link>
